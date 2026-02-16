ec3ec2ffdc3ce302405490537d6a6774349a2922
jmlaur <103467834+jmlaur@users.noreply.github.com>
2026-02-16 17:49:21 -0500

    Update presentation_mode_v2.launch.py

--- src/core/maki_operational_nodes/launch/presentation_mode_v2.launch.py
+++ src/core/maki_operational_nodes/launch/presentation_mode_v2.launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
             "bash",
             "-lc",
             shell_prefix
-            + "cd ~/makimate-2026/src/server_llm/server_llm && "
+            + "cd ~/makimate-2026/src/core/server_llm/server_llm && "
               "python llm_bridge_node.py "
               "--ros-args "
               "-p laptop_host:='http://35.50.73.78:8000' "
